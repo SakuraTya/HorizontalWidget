29dd3ba8ceab344ec1941bd1b102acf6e1d39321
Bob Yuan <yuanbo@ismartv.cn>
2012-11-09 19:42:02 +0800

    update HGridList

--- HorizontalWidget/src/org/sakuratya/horizontal/ui/HGridView.java
+++ HorizontalWidget/src/org/sakuratya/horizontal/ui/HGridView.java
@@ -5,6 +5,7 @@ import javax.crypto.spec.PSource;
 import org.sakuratya.horizontal.adapter.HGridAdapter;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.FloatMath;
 import android.view.KeyEvent;
@@ -27,7 +28,7 @@ public class HGridView extends AdapterView<HGridAdapter> {
 	
 	private static final int INVALID_POSITION = -1;
 	private int mSelectedPosition;
-	private int mNextPosition;
+	private int mNextSelectedPosition;
 	
 	/**
 	 * Set rows per column.
@@ -37,7 +38,28 @@ public class HGridView extends AdapterView<HGridAdapter> {
 	private Scroller mScroller;
 	private int mScrollAmount = 0;
 	private int mCurrentX;
+	
+	private int mNextX;
+	
+	private Rect mListPadding;
+	
+	private int mSelectorLeftPadding;
+	private int mSelectorTopPadding;
+	private int mSelectorRightPadding;
+	private int mSelectorBottomPadding;
+	/**
+     * When arrow scrolling, need a certain amount of pixels to preview next
+     * items.  This is usually the fading edge, but if that is small enough,
+     * we want to make sure we preview at least this many pixels.
+     */
+    private static final int MIN_SCROLL_PREVIEW_PIXELS = 2;
 
+    /**
+     * When arrow scrolling, ListView will never scroll more than this factor
+     * times the height of the list.
+     */
+    private static final float MAX_SCROLL_FACTOR = 0.33f;
+    
 	public HGridView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		// TODO Auto-generated constructor stub
@@ -79,6 +101,11 @@ public class HGridView extends AdapterView<HGridAdapter> {
 		
 	}
 	
+	
+	private void setNextSelectionInt(int position) {
+		mNextSelectedPosition = position;
+	}
+	
 	@Override
 	protected void onLayout(boolean changed, int left, int top, int right,
 			int bottom) {
@@ -88,8 +115,12 @@ public class HGridView extends AdapterView<HGridAdapter> {
 
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		// TODO Auto-generated method stub
-		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		
+		final Rect listPadding = mListPadding;
+		listPadding.left = mSelectorLeftPadding + getPaddingLeft();
+		listPadding.top = mSelectorTopPadding + getPaddingTop();
+		listPadding.right = mSelectorRightPadding + getPaddingRight();
+		listPadding.bottom = mSelectorBottomPadding + getPaddingBottom();
 	}
 	
 	@Override
@@ -147,23 +178,68 @@ public class HGridView extends AdapterView<HGridAdapter> {
 	private boolean arrowScroll(int direction) {
 		
 		View selectedView = getSelectedView();
-		mNextPosition = lookForSelectablePosition(direction);
+		int nextSelectedPosition = lookForSelectablePositionOnScreen(direction);
+		int amountToScroll = amountToScroll(direction, nextSelectedPosition);
+		
+		if(nextSelectedPosition!=INVALID_POSITION) {
+			setNextSelectionInt(nextSelectedPosition);
+		}
+		if(amountToScroll>0) {
+			doScroll(direction==FOCUS_RIGHT?amountToScroll:-amountToScroll);
+		}
 		return false;
 	}
 	
-	private int lookForSelectablePosition(int direction) {
+	private void doScroll(int delta) {
+		if(delta!=0) {
+			scrollBy(delta, 0);
+		}
+	}
+	
+	
+	private int lookForSelectablePositionOnScreen(int direction) {
 		final int firstPosition = mFirstPosition;
-		final int currentSelectedColumn = getColumn(mSelectedPosition);
-		final int currentSelectedRow = getRow(mSelectedPosition);
-		switch(direction) {
-		case FOCUS_LEFT:
-			break;
-		case FOCUS_RIGHT:
-			break;
-		case FOCUS_UP:
-			break;
-		case FOCUS_DOWN:
-			break;
+		
+		if(direction== FOCUS_LEFT) {
+			final int lastVisiblePosition = mFirstPosition + getChildCount() - 1;
+			final int currentSelectedColumn = mSelectedPosition==INVALID_POSITION ? getColumn(lastVisiblePosition): getColumn(mSelectedPosition);
+			final int firstColumn = getColumn(firstPosition);
+			if(currentSelectedColumn<=0 && currentSelectedColumn < firstColumn) {
+				return INVALID_POSITION;
+			}
+			final int currentSelectedRow =mSelectedPosition==INVALID_POSITION ? 0 : getRow(mSelectedPosition);
+			for(int col=currentSelectedColumn-1; col>=firstColumn;col--) {
+				int[] positionRange = getPositionRangeByColumn(col);
+				// Skip the separator
+				if(mAdapter.isEnabled(positionRange[0])) {
+					// If next column has less rows than currentSelectedRow, just return the last row of next column. 
+					return currentSelectedRow > positionRange[1] ? positionRange[1] : positionRange[0] + currentSelectedRow;
+				}
+			}
+			
+		} else if(direction==FOCUS_RIGHT) {
+			final int lastVisiblePosition = mFirstPosition + getChildCount() - 1;
+			if(mSelectedPosition >= lastVisiblePosition) {
+				return INVALID_POSITION;
+			}
+			final int currentSelectedColumn = mSelectedPosition==INVALID_POSITION ? getColumn(firstPosition): getColumn(mSelectedPosition);
+			final int lastVisibleColumn = getColumn(lastVisiblePosition);
+			if(currentSelectedColumn >= lastVisibleColumn) {
+				return INVALID_POSITION;
+			}
+			final int currentSelectedRow =mSelectedPosition==INVALID_POSITION ? 0 : getRow(mSelectedPosition);
+			for(int col=currentSelectedColumn+1; col<=lastVisibleColumn;col++) {
+				int[] positionRange = getPositionRangeByColumn(col);
+				// Skip the separator
+				if(mAdapter.isEnabled(positionRange[0])) {
+					// If next column has less rows than currentSelectedRow, just return the last row of next column. 
+					return currentSelectedRow > positionRange[1] ? positionRange[1] : positionRange[0] + currentSelectedRow;
+				}
+			}
+		} else if(direction==FOCUS_UP) {
+			
+		} else {
+			
 		}
 		return INVALID_POSITION;
 	}
@@ -254,10 +330,85 @@ public class HGridView extends AdapterView<HGridAdapter> {
 		positionRange[1] = positionEnd;
 		return positionRange;
 	}
+	
+	private int amountToScroll(int direction, int nextSelectedPosition) {
+		final int listRight = getWidth() + mListPadding.right;
+		final int listLeft = mListPadding.left;
+		
+		if(direction==FOCUS_RIGHT) {
+			int indexToMakeVisible = getChildCount() - 1;
+			if(nextSelectedPosition!=INVALID_POSITION) {
+				indexToMakeVisible = nextSelectedPosition - mFirstPosition;
+			}
+			final int positionToMakeVisible = indexToMakeVisible + mFirstPosition;
+			final View viewToMakeVisible = getChildAt(indexToMakeVisible);
+			
+			int goalRight = listRight;
+			if(positionToMakeVisible < mAdapter.getCount() -1) {
+				goalRight -= getArrowScrollPreviewLength();
+			}
+			
+			if(viewToMakeVisible.getRight() < goalRight) {
+				return 0;
+			}
+			
+			if(nextSelectedPosition!=INVALID_POSITION && goalRight - viewToMakeVisible.getLeft() >  getMaxScrollAmount()) {
+				// item already has enough of it visible, changing selection is good enough
+				return 0;
+			}
+			
+			int amountToScroll = viewToMakeVisible.getRight() - goalRight;
+			if(mFirstPosition+getChildCount()==mAdapter.getCount()) {
+				// Make sure we will not scroll over the last item.
+				final int max = getChildAt(mFirstPosition+getChildCount()-1).getRight() - goalRight;
+				amountToScroll = Math.min(max, amountToScroll);
+			}
+			return Math.min(amountToScroll, getMaxScrollAmount());
+		} else {
+			int indexToMakeVisible = 0;
+			if(nextSelectedPosition!=INVALID_POSITION) {
+				indexToMakeVisible = nextSelectedPosition - mFirstPosition;
+			}
+			
+			final int positionToMakeVisible = indexToMakeVisible + mFirstPosition;
+			final View viewToMakeVisible = getChildAt(indexToMakeVisible);
+			
+			int goalLeft = listLeft;
+			if(positionToMakeVisible > 0) {
+				goalLeft += getArrowScrollPreviewLength();
+			}
+			
+			if(viewToMakeVisible.getLeft() > goalLeft) {
+				return 0;
+			}
+			
+			if(nextSelectedPosition!=INVALID_POSITION && viewToMakeVisible.getRight() - goalLeft > getMaxScrollAmount()) {
+				// item already has enough of it visible, changing selection is good enough
+				return 0;
+			}
+			
+			int amountToScroll = goalLeft - viewToMakeVisible.getLeft();
+			
+			if(mFirstPosition == 0) {
+				// Make sure we will not scroll over the last item.
+				final int max = getChildAt(0).getLeft() - goalLeft;
+				amountToScroll = Math.min(max, amountToScroll);
+			}
+			return Math.min(amountToScroll, getMaxScrollAmount());
+		}
+	}
 
+	private int getArrowScrollPreviewLength() {
+		return Math.max(MIN_SCROLL_PREVIEW_PIXELS, getHorizontalFadingEdgeLength());
+	}
+	
+	private int getMaxScrollAmount() {
+		return (int) (MAX_SCROLL_FACTOR * (getRight() - getLeft()));
+	}
+	
 	private void resetList() {
 		mSelectedPosition = INVALID_POSITION;
-		mNextPosition = INVALID_POSITION;
+		mNextSelectedPosition = INVALID_POSITION;
 		mFirstPosition = 0;
 		
 	}
