f16c88139e95f9e8e074fb5244744cd9798a84f7
Bob Yuan <yuanbo@ismartv.cn>
2012-11-16 18:35:22 +0800

    writing setupChild method

--- HorizontalWidget/src/org/sakuratya/horizontal/ui/HGridView.java
+++ HorizontalWidget/src/org/sakuratya/horizontal/ui/HGridView.java
@@ -1,5 +1,7 @@
 package org.sakuratya.horizontal.ui;
 
+import java.util.ArrayList;
+
 import javax.crypto.spec.PSource;
 
 import org.sakuratya.horizontal.adapter.HGridAdapter;
@@ -10,6 +12,7 @@ import android.util.AttributeSet;
 import android.util.FloatMath;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.Scroller;
@@ -23,30 +26,6 @@ import android.widget.Scroller;
  */
 public class HGridView extends AdapterView<HGridAdapter> {
 	
-	
-	private HGridAdapter mAdapter;
-	
-	private static final int INVALID_POSITION = -1;
-	private int mSelectedPosition;
-	private int mNextSelectedPosition;
-	
-	/**
-	 * Set rows per column.
-	 */
-	private int mRows;
-	private int mFirstPosition;
-	private Scroller mScroller;
-	private int mScrollAmount = 0;
-	private int mCurrentX;
-	
-	private int mNextX;
-	
-	private Rect mListPadding;
-	
-	private int mSelectorLeftPadding;
-	private int mSelectorTopPadding;
-	private int mSelectorRightPadding;
-	private int mSelectorBottomPadding;
 	/**
      * When arrow scrolling, need a certain amount of pixels to preview next
      * items.  This is usually the fading edge, but if that is small enough,
@@ -59,6 +38,59 @@ public class HGridView extends AdapterView<HGridAdapter> {
      * times the height of the list.
      */
     private static final float MAX_SCROLL_FACTOR = 0.33f;
+    
+    private static final int LAYOUT_SET_SELECTION = 0;
+    private static final int LAYOUT_NORMAL = 1;
+    private static final int LAYOUT_MOVE_SELECTION = 2;
+    
+	private HGridAdapter mAdapter;
+	
+	private static final int INVALID_POSITION = -1;
+	private int mSelectedPosition;
+	private int mNextSelectedPosition;
+	
+	private int mAmountToScroll = 0;
+	/**
+	 * Set rows per column.
+	 */
+	private int mRows;
+	/**
+	 * The position in adapter which is the first child of current list.
+	 */
+	private int mFirstPosition;
+	
+	private Rect mListPadding;
+	
+	private int mSelectorLeftPadding;
+	private int mSelectorTopPadding;
+	private int mSelectorRightPadding;
+	private int mSelectorBottomPadding;
+	
+	private boolean isInLayout = false;
+	
+	private final RecycleBin mRecycler = new RecycleBin();
+	
+    /**
+     * The horizontal space between each item.
+     */
+    private int mHorizontalSpace;
+    /**
+     * THe vertical space between each item.
+     */
+    private int mVerticalSpace;
+   
+    private int mRowHeight;
+    /**
+     * current layout mode.
+     */
+    private int mLayoutMode;
+    
+    private boolean mDataChanged = false;
+    
+    private View mReferenceViewInSelectedColumn = null;
+    private View mReferenceView = null;
+    
+    final boolean[] mIsScrap = new boolean[1];
     
 	public HGridView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
@@ -106,11 +138,225 @@ public class HGridView extends AdapterView<HGridAdapter> {
 		mNextSelectedPosition = position;
 	}
 	
+//	private void scrollListItemsBy(int delta) {
+//		final int listRight = getRight() - mListPadding.right;
+//		final int listLeft = mListPadding.left;
+//		final RecycleBin recycleBin = mRecycler;
+//		if(delta < 0) {
+//			// May need to pan views into right space.
+//			int numChildren = getChildCount();
+//			View last = getChildAt(numChildren -1);
+//			int lastVisiblePosition = mFirstPosition + numChildren -1;
+//			while(last.getLeft() < listRight) {
+//				if(lastVisiblePosition < mAdapter.getCount() -1) {
+//					last = addColumnRight(last, lastVisiblePosition);
+//					numChildren = getChildCount();
+//					lastVisiblePosition = mFirstPosition + numChildren -1;
+//				} else {
+//					break;
+//				}
+//			}
+//			// We must ensure the last view is not a separator. But two separator without a selectable view
+//			// between them is invalid, we can't ensure it will work properly
+//			if(!last.isEnabled() && lastVisiblePosition + 1 < mAdapter.getCount()) {
+//				addColumnRight(last, lastVisiblePosition);
+//			}
+//			
+//			// May need to pan off views left which is out bound of screen. And fully invisible.
+//			View first = getChildAt(0);
+//			int nextPosition = mFirstPosition + 1;
+//			// We must ensure the first view is not a separator.
+//			while(first.getRight() < listLeft && mAdapter.isEnabled(nextPosition)) {
+//				detachViewFromParent(first);
+//				recycleBin.addScrapView(first);
+//				first = getChildAt(0);
+//				mFirstPosition++;
+//				nextPosition = mFirstPosition + 1;
+//			}
+//		} else {
+////			View first = getChildAt(0);
+//			
+//		}
+//	}
+	
+	private View fillLeft(int pos, int nextRight) {
+		return null;
+	}
+	
+	private View fillRight(int pos, int nextLeft) {
+		return null;
+	}
+	
+	private View moveSelection(int amount, int childrenLeft, int childrenRight) {
+		final int fadingEdgeLength = getHorizontalFadingEdgeLength();
+		final int selectedPosition = mSelectedPosition;
+		final int nextSelectedPosition = mNextSelectedPosition;
+		final int horizontalSpace = mHorizontalSpace;
+		
+		View selectedView;
+		int oldCol = getColumn(selectedPosition);
+		int col = getColumn(nextSelectedPosition);
+		
+		if(amount >0) {
+			final int oldRight = mReferenceViewInSelectedColumn==null?0:mReferenceViewInSelectedColumn.getRight();
+			selectedView = makeColumn(col, oldRight);
+			
+		} else if(amount<0) {
+			
+		} else {
+			
+		}
+		return null;
+	}
+
+	private View makeColumn(int column, int x) {
+		final int firstPosition = mFirstPosition;
+		final int verticalSpacing = mVerticalSpace;
+		int[] positionRange = getPositionRangeByColumn(column);
+		View selectedView = null;
+		View child = null;
+		int nextTop = mListPadding.top + verticalSpacing;
+		for(int pos = positionRange[0]; pos <= positionRange[1];pos++) {
+			boolean selected = pos == firstPosition;
+			
+			final int where = pos - positionRange[0];
+			child = makeAndAddView(pos, x, nextTop, selected, where);
+			
+			nextTop += mRowHeight;
+			if(pos < positionRange[1]) {
+				nextTop += verticalSpacing;
+			}
+			if(selected) {
+				selectedView = child;
+				
+			}
+		}
+		
+		mReferenceView = child;
+		if(selectedView!=null) {
+			mReferenceViewInSelectedColumn = mReferenceView;
+		}
+		return selectedView;
+	}
+	
+	private View makeAndAddView(int position, int x, int childrenTop, boolean selected, int where) {
+		View child;
+		if(!mDataChanged) {
+			// Try to use an existing view.
+			child = mRecycler.getActiveView(position);
+			if(child!=null) {
+				// Found it! We are using an existing view.
+				// Just need to position it.
+				setupChild(child, position, x, childrenTop, selected, true, where);
+				return child;
+			}
+			
+		}
+		// Make a new view for the position, or convert an old view.
+		child = obtainView(position, mIsScrap);
+		setupChild(child, position, x, childrenTop, selected, mIsScrap[0], where);
+		return child;
+	}
+	
+	private void setupChild(View child, int position, int x, int childrenTop, boolean selected, boolean recycled, int where) {
+		boolean isSelected = selected && shouldShowSelector();
+		boolean updateChildSelected = isSelected == child.isSelected();
+		boolean needToMeasure = !recycled || updateChildSelected || child.isLayoutRequested();
+		LayoutParams p = (LayoutParams) child.getLayoutParams();
+		if(p == null) {
+			p = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+		}
+		
+		if(recycled) {
+			attachViewToParent(child, where, p);
+		} else {
+			addViewInLayout(child, where, p, true);
+		}
+		if(updateChildSelected) {
+			child.setSelected(isSelected);
+			if(isSelected) {
+				requestFocus();
+			}
+		}
+		
+		
+	}
+	
+	/**
+	 * Get a view and have it show data associated with its position.
+	 * This is called when we discovered that the view is already not available for reuse in the recycle bin.
+	 * The only choices left are converting an old view or make a new one. 
+	 * @param position
+	 * @return
+	 */
+	protected View obtainView(int position, boolean[] isScrap) {
+		isScrap[0] = false;
+		View scrapView;
+		scrapView = mRecycler.getScrapView(position);
+		
+		View child;
+		if(scrapView!=null) {
+			child = mAdapter.getView(position, scrapView, this);
+			if(child==scrapView) {
+				isScrap[0] = true;
+				child.onFinishTemporaryDetach();
+			} else {
+				mRecycler.addScrapView(scrapView);
+			}
+		} else {
+			child = mAdapter.getView(position, null, this);
+		}
+		return this;
+	}
+	
 	@Override
 	protected void onLayout(boolean changed, int left, int top, int right,
 			int bottom) {
-		// TODO Auto-generated method stub
 		super.onLayout(changed, left, top, right, bottom);
+		layoutChildren();
+	}
+	
+	protected void layoutChildren() {
+		
+		if(isInLayout) {
+			return;
+		}
+		isInLayout = true;
+		
+		try {
+			if(mAdapter==null) {
+				resetList();
+				return;
+			}
+			final int childrenLeft = mListPadding.left;
+			final int childrenRight = getRight() - getLeft() - mListPadding.right;
+			final RecycleBin recycleBin = mRecycler;
+			final int firstPosition = mFirstPosition;
+			int childCount = getChildCount();
+			
+			View sel;
+			
+			if(mDataChanged) {
+				for(int i=0; i<childCount;i++) {
+					recycleBin.addScrapView(getChildAt(i));
+				}
+			} else {
+				recycleBin.fillActiveViews(childCount, firstPosition);
+			}
+			
+			detachAllViewsFromParent();
+			
+			switch(mLayoutMode) {
+			case LAYOUT_NORMAL:
+				break;
+			case LAYOUT_MOVE_SELECTION:
+				sel = moveSelection(mAmountToScroll, childrenLeft, childrenRight);
+				break;
+			}
+		} finally {
+			isInLayout = false;
+			mAmountToScroll = 0;
+		}
 	}
 
 	@Override
@@ -149,6 +395,12 @@ public class HGridView extends AdapterView<HGridAdapter> {
 					handled = seekForOtherView(FOCUS_RIGHT);
 				}
 				break;
+			case KeyEvent.KEYCODE_DPAD_LEFT:
+				handled = arrowScroll(FOCUS_LEFT);
+				if(!handled) {
+					handled = seekForOtherView(FOCUS_RIGHT);
+				}
+				break;
 			}
 		}
 		switch(action) {
@@ -174,24 +426,37 @@ public class HGridView extends AdapterView<HGridAdapter> {
 	 */
 	private boolean arrowScroll(int direction) {
 		
-		View selectedView = getSelectedView();
 		int nextSelectedPosition = lookForSelectablePositionOnScreen(direction);
+		
+		if(nextSelectedPosition==INVALID_POSITION) {
+			return false;
+		}
+		
 		int amountToScroll = amountToScroll(direction, nextSelectedPosition);
 		
 		if(nextSelectedPosition!=INVALID_POSITION) {
 			setNextSelectionInt(nextSelectedPosition);
 		}
 		if(amountToScroll>0) {
-			doScroll(direction==FOCUS_RIGHT?amountToScroll:-amountToScroll);
+			mAmountToScroll = amountToScroll;
+			
+//			scrollListItemsBy(direction==FOCUS_RIGHT?amountToScroll:-amountToScroll);
+		} else {
+			mAmountToScroll = 0;
 		}
-		return false;
-	}
-	
-	private void doScroll(int delta) {
-		if(delta!=0) {
-			scrollBy(delta, 0);
+		if(nextSelectedPosition!=INVALID_POSITION) {
+			mLayoutMode = LAYOUT_MOVE_SELECTION;
+			layoutChildren();
 		}
+		
+		return true;
 	}
+//	
+//	private void doScroll(int delta) {
+//		if(delta!=0) {
+//			scrollBy(delta, 0);
+//		}
+//	}
 	
 	
 	private int lookForSelectablePositionOnScreen(int direction) {
@@ -337,6 +602,7 @@ public class HGridView extends AdapterView<HGridAdapter> {
 			if(nextSelectedPosition!=INVALID_POSITION) {
 				indexToMakeVisible = nextSelectedPosition - mFirstPosition;
 			}
+			
 			final int positionToMakeVisible = indexToMakeVisible + mFirstPosition;
 			final View viewToMakeVisible = getChildAt(indexToMakeVisible);
 			
@@ -410,8 +676,64 @@ public class HGridView extends AdapterView<HGridAdapter> {
 		
 	}
 	
-	private void makeRow() {
+	public void setRowHeight(int height) {
+		mRowHeight = height;
+	}
+	
+	public int getRowHeight() {
+		return mRowHeight;
+	}
+	
+	protected boolean shouldShowSelector() {
+		return hasFocus();
+	}
+	
+	
+	static class LayoutParams extends ViewGroup.LayoutParams {
+
+		public LayoutParams(Context arg0, AttributeSet arg1) {
+			super(arg0, arg1);
+			// TODO Auto-generated constructor stub
+		}
+
+		public LayoutParams(int arg0, int arg1) {
+			super(arg0, arg1);
+			// TODO Auto-generated constructor stub
+		}
+
+		public LayoutParams(android.view.ViewGroup.LayoutParams arg0) {
+			super(arg0);
+			// TODO Auto-generated constructor stub
+		}
 		
+	}
+	
+	class RecycleBin {
+		
+		private int firstActivePosition;
+		
+		private ArrayList<View> activeViews;
+		
+		private ArrayList<View> mScrapViews;
+		
+		private void fillActiveViews(int childCount, int firstPosition) {
+			
+		}
+		
+		private View getActiveView(int position) {
+			return null;
+		}
+		public void addScrapView(View scrap) {
+			
+		}
+		
+		public View getScrapView(int position) {
+			return null;
+		}
+		
+		public void clear() {
+			
+		}
 	}
 
 }
